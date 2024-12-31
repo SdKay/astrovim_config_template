cd516c1148e5b6aaf65b2972ddb9f2668d085e2e
silas.xing <silas_xing@realsil.com.cn>
2024-12-31 15:40:01 +0800

    update config

--- lua/community.lua
+++ lua/community.lua
@@ -8,9 +8,16 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.editing-support.vim-doge" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
-  --{ import = "astrocommunity.recipes.disable-tabline" },
+
+  { import = "astrocommunity.editing-support.nvim-treesitter-context" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+  { import = "astrocommunity.recipes.auto-session-restore" },
+
+  -- { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+  -- { import = "astrocommunity.indent.indent-blankline-nvim" },
+  -- { import = "astrocommunity.recipes.disable-tabline" },
   -- import/override with your plugins folder
 }
--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -27,11 +27,22 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        jumpoptions = "stack",
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        list = true,
+        listchars={
+          --tab = '▏→',
+          tab = '▏»',
+          eol = '↲',
+          trail = '·',
+          extends = '>',
+          precedes = '<',
+          space = '·'
+        },
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -59,6 +70,14 @@ return {
           end,
           desc = "Close buffer from tabline",
         },
+        ["<Leader>fl"] = {
+          function()
+            require("telescope").extensions.live_grep_args.live_grep_args()
+          end,
+          desc = "Telescope live grep with args",
+        },
+
+        ["<Leader>go"] = { function() require("gitsigns").toggle_current_line_blame() end, desc = "Toggle current line git blame" },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
